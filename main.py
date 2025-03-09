32a19211d0d0153237f3f70f0f28f334ed46fc66
Aayam Raj Shakya <aayamrajshakya@pm.me>
2025-03-09 15:22:12 -0500

    Battlesnake execution file

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "Jack Sparrow",
+        "color": "#abb1b6",
+        "head": "pirate",
+        "tail": "pirate",
     }
 
 
